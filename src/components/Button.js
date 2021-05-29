b882945ac018733f3306f2ac5997c0ef1c7ecb31
m-adnan-farooq <m.adnan.far@gmail.com>
2021-05-29 17:28:28 +0500

    A Starter pack for a Drop down Navigation Bar

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Button.css'
+
+export const Button = () => {
+    return (
+        <Link to='sign-up'>
+            <button className='btn'>
+                Sign Up
+            </button>
+        </Link>
+    )
+}
+
